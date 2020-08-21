8693ed95dfbc78e966d27a0de2e60241a991d711
v-juma1 <ma_jun_1023@163.com>
2020-08-21 17:43:28 +0800

    linear Regression model for boston data

---
+++ src/linearRegression_boston.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
+"""
+sklearn 自带的波士顿房价数据集,13维特征
+"""
+
+boston= load_boston()
+x=boston.data
+y=boston.target
+
+#%20的数据作为测试数据
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=3)
+
+#多项式特征
+def polynomial_feature(degree=1):
+    pf=PolynomialFeatures(degree=degree,include_bias=False)
+    lr=LinearRegression(normalize=True)
+    pipe=Pipeline([("polynomial_features",pf),("linear_regression",lr)])
+    return pipe
+
+model=polynomial_feature(degree=2)
+model.fit(x_train,y_train)
+score=model.score(x_test,y_test)
+print(score)
+
