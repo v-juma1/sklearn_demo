4a283da36b26717371cba56a7984a7bc9bc19c3b
v-juma1 <13681144834@163.com>
2023-07-28 20:00:12 +0800

    add svm and decisiontree

--- src/decision_tree_iris.py
+++ src/decision_tree_iris.py
@@ -11,11 +11,97 @@ logging.config.fileConfig("scikit-learn/conf/logging.conf", encoding="utf8")
 logger = logging.getLogger("fileAndConsole")
 
 from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+import dtreeviz
+
+
+def load_data():
+    iris = load_iris()
+    #features = list(iris.feature_names)
+    #class_names = iris.target_names
+
+    X = iris.data
+    y = iris.target
+
+    return X, y
+
+
+def fit_classifier(X, y):
+    features = [
+        'sepal length (cm):花萼长度', 'sepal width (cm):花萼宽度',
+        'petal length (cm):花瓣长度', 'petal width (cm):花瓣宽度'
+    ]
+    class_names = ['setosa:山鸢尾', 'versicolor:变色鸢尾', 'virginica:维吉尼亚鸢尾']
+    iris_classifier = DecisionTreeClassifier(max_depth=None,
+                                             min_samples_leaf=1,
+                                             random_state=42)
+    iris_classifier.fit(X, y)
+
+    # 可视化
+    viz_model = dtreeviz.model(iris_classifier,
+                               X_train=X,
+                               y_train=y,
+                               feature_names=features,
+                               target_name='iris',
+                               class_names=class_names)
+
+    viz_model.view(
+        scale=0.8,
+        fontname="KaiTi").save("scikit-learn/log/classifier_iris.svg")
+
+    # 预测或者输出概率
+    one_data = [7.0, 3.2, 4.7, 1.4]
+    logger.info(iris_classifier.predict([one_data]))
+
+    # 可视化一条数据被分类的过程
+    viz_model.view(
+        scale=0.8, x=one_data,
+        fontname="KaiTi").save("scikit-learn/log/classifier_one_iris.svg")
+
+    # 可视化一条数据被分类的过程，只画出分类过程中用到的分支，其他分支忽略
+    viz_model.view(scale=0.8,
+                   x=one_data,
+                   fontname="KaiTi",
+                   show_just_path=True).save(
+                       "scikit-learn/log/classifier_onepath_iris.svg")
+
+    # 现在分类树是最大生长，没有剪枝，所以输出的概率数组中，只有一个为1，其他为0
+    # 剪枝（如设置max_depth）以后，概率才会分布到数组中其他位置
+    logger.info(iris_classifier.predict_proba([[7.0, 3.2, 4.7, 1.4]]))
+
+
+def fit_regressor(X, y):
+    features = [
+        'sepal length (cm):花萼长度', 'sepal width (cm):花萼宽度',
+        'petal length (cm):花瓣长度', 'petal width (cm):花瓣宽度'
+    ]
+    class_names = ['setosa:山鸢尾', 'versicolor:变色鸢尾', 'virginica:维吉尼亚鸢尾']
+    iris_regressor = DecisionTreeRegressor(max_depth=None,
+                                           min_samples_leaf=1,
+                                           random_state=42)
+    iris_regressor.fit(X, y)
+
+    # 可视化
+    viz_model = dtreeviz.model(iris_regressor,
+                               X_train=X,
+                               y_train=y,
+                               feature_names=features,
+                               target_name='iris',
+                               class_names=class_names)
+
+    viz_model.view(
+        scale=0.8,
+        fontname="KaiTi").save("scikit-learn/log/regressor_iris.svg")
+
+    # 预测
+    logger.info(iris_regressor.predict([[7.0, 3.2, 4.7, 1.4]]))
 
 
 def run():
-    pass
+    X, y = load_data()
+
+    fit_classifier(X, y)
+    fit_regressor(X, y)
 
 
 if __name__ == "__main__":
--- src/decision_tree_titanic.py
+++ src/decision_tree_titanic.py
@@ -20,7 +20,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV
 
 
-#读取数据集
+#读取数据集,并进行预处理
 def read_dataset(fname):
     # 指定第一列作为行索引
     data = pd.read_csv(fname, index_col=0)
@@ -33,35 +33,16 @@ def read_dataset(fname):
     data['Embarked'] = data['Embarked'].apply(lambda n: labels.index(n))
     # 处理缺失数据
     data = data.fillna(0)
-    return data
 
+    y = data["Survived"].values
+    x = data.drop(["Survived"], axis=1).values
+    return x, y
 
-train = read_dataset('scikit-learn/data/titanic/train.csv')
-
-y = train["Survived"].values
-x = train.drop(["Survived"], axis=1).values
-
-#分割数据x
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-
-#DecisionTreeClassifier()模型常见参数
-# criterion="gini", 特征选择算法，gini(基尼不纯度)或者entropy(信息熵),两者差异不大
-# splitter="best",  best ：选择最优的分支创建，random：在排名靠前的特征种随机选择一个来创建
-# max_depth=None, 决策树的最大深度
-# min_samples_split=2, 能创建分支的数据集大小，一个节点的数据样本小于这个数，将不再创建分支
-# min_samples_leaf=1, 创建分支后，节点的样本量必须大于或者等于这个值，否则将不再创建分支
-# min_weight_fraction_leaf=0.,限制了叶子节点所有样本权重和的最小值，如果小于这个值，则会和兄弟节点一起被剪枝
-# max_leaf_nodes=None, 最大的样本节点个数
-# min_impurity_split=None, 即将废弃,信息增益（划分数据集前后信息熵的变化值）的阈值，小于这个阈值将不创建分支
-# min_impurity_decrease,代替min_impurity_split作为信息增益的参数
-# class_weight=None, 指定各类样本的权重
 
 #模型的优化方法，选择某个参数，在一组范围确定这个参数的最优值
-#比如max_depth（模型深度），min_impurity_split（信息增益的阈值），确定这两个参数的最优值，并且画出其和模型评分的关系
-
-
+#比如max_depth（模型深度）
 #找出得分最高的max_depth，并画出其和模型评分的关系
-def max_depth_score():
+def max_depth_score(x_train, x_test, y_train, y_test):
 
     def cv_score(d):
         clf = DecisionTreeClassifier(max_depth=d)
@@ -92,11 +73,12 @@ def max_depth_score():
     plt.savefig("scikit-learn/log/max_depth_score.png")
 
 
-#找出得分最高的min_impurity_split，并画出其和模型评分的关系
-def min_impurity_split_score():
+#找出得分最高的min_impurity_decrease（信息增益的阈值），并画出其和模型评分的关系
+def min_impurity_decrease_score(x_train, x_test, y_train, y_test):
 
     def cv_score(val):
-        clf = DecisionTreeClassifier(criterion="gini", min_impurity_split=val)
+        clf = DecisionTreeClassifier(criterion="gini",
+                                     min_impurity_decrease=val)
         clf.fit(x_train, y_train)
         train_score = clf.score(x_train, y_train)
         test_score = clf.score(x_test, y_test)
@@ -111,26 +93,24 @@ def min_impurity_split_score():
     best_score_index = np.argmax(test_scores)
     best_score = test_scores[best_score_index]
     best_param = values[best_score_index]
-    #print("min_impurity_split:{},score:{}".format(best_param,best_score)
+    #print("min_impurity_decrease:{},score:{}".format(best_param,best_score)
 
     #画出关系图
     plt.figure(figsize=(6, 4), dpi=144)
     plt.grid()
-    plt.xlabel("min_impurity_split of decision tree")
+    plt.xlabel("min_impurity_decrease of decision tree")
     plt.ylabel("score")
     plt.plot(values, train_scores, '.g-', label='train score')
     plt.plot(values, test_scores, '.r--', label="test score")
     plt.legend()
-    plt.savefig("scikit-learn/log/min_impurity_split_score.png")
+    plt.savefig("scikit-learn/log/min_impurity_decrease_score.png")
 
 
 #以上挨个寻找最优参数的方法有两个问题
 # 1. 数据不稳定
 # 2. 不能一次选择多个参数最优的参数组合
 # 使用sklean的模型参数选择工具包GridSearchCV
-
-
-def GridSearchCV_curve():
+def GridSearchCV_curve(x_train, x_test, y_train, y_test):
     #画出模型参数于模型评分的关系图
     def plot_curve(train_size, cv_result, xlabel):
         mean_train = cv_result["mean_train_score"]
@@ -172,10 +152,7 @@ def GridSearchCV_curve():
 
     space = np.linspace(0, 0.5, 50)
 
-    #多个参数同时调优，可以用列表的形式：
-    # params=[{'criterion': ['entropy'], 'min_impurity_decrease': entropy_thresholds},{'criterion': ['gini'], 'min_impurity_decrease': gini_thresholds},
-    #   {'max_depth': range(2, 10)},{'min_samples_split': range(2, 30, 2)}]
-    params = {'min_impurity_split': space}
+    params = {'min_impurity_decrease': space}
 
     #GridSearchCV会根据params中的值来构建模型，其中并计算平均分和标准差，cv=5表示每次计算把数据分为5份，一份测试，其他用来训练
     #得到的最优参数及评分保存在clf.best_params_和clf.best_score_中，clf.cv_results_保存了计算过程中的中间结果
@@ -183,12 +160,23 @@ def GridSearchCV_curve():
                        params,
                        cv=5,
                        return_train_score=True)
-    clf.fit(x, y)
+    clf.fit(x_train, y_train)
     print("params:{0}\n score:{1}".format(clf.best_params_, clf.best_score_))
 
     plot_curve(space, clf.cv_results_, xlabel='gini thresholds')
 
 
-max_depth_score()
-min_impurity_split_score
-GridSearchCV_curve()
+def run():
+    x, y = read_dataset('scikit-learn/data/titanic/train.csv')
+    #分割数据x
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+    max_depth_score(x_train, x_test, y_train, y_test)
+
+    min_impurity_decrease_score(x_train, x_test, y_train, y_test)
+
+    GridSearchCV_curve(x_train, x_test, y_train, y_test)
+
+
+if __name__ == "__main__":
+    run()
